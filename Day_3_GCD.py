580a83d8837035ba26a19d87ce34f12c1ad41864
246eswar <104315271+246eswar@users.noreply.github.com>
2025-01-04 13:13:10 +0530

    Add files via upload

---
+++ Day_3_GCD.py
@@ -0,0 +1,23 @@
+'''The greatest common divisor (GCD)
+of two numbers is the largest positive
+integer that divides both numbers without leaving a remainder.'''
+
+#GCD of two numbers
+a=int(input("enter the first number : "))
+b=int(input("enter the second number : "))
+while b != 0:
+    a,b=b,a%b #a=b,b==a%b
+    
+print("Gcd is : ",a)
+
+#2nd method
+a=int(input("enter the first number : "))
+b=int(input("enter the second number : "))
+while b != 0:
+    temp=b
+    b=a%b
+    a=temp     
+print("Gcd is : ",a)
+
+#a=12,b=14
+#in while temp=14,b=2 and a=b so gcd is 2
