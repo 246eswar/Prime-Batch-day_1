ae4b866f20a325ab985dae3582d4f3e5cd96a5ae
246eswar <104315271+246eswar@users.noreply.github.com>
2025-01-08 16:19:47 +0530

    Update Day_3_GCD.py

--- Day_3_GCD.py
+++ Day_3_GCD.py
@@ -5,6 +5,7 @@ integer that divides both numbers without leaving a remainder.'''
 #GCD of two numbers
 a=int(input("enter the first number : "))
 b=int(input("enter the second number : "))
+
 while b != 0:
     a,b=b,a%b #a=b,b==a%b
     
@@ -21,3 +22,14 @@ print("Gcd is : ",a)
 
 #a=12,b=14
 #in while temp=14,b=2 and a=b so gcd is 2
+
+#if zerogiven
+a=int(input("enter the first number : "))
+b=int(input("enter the second number : "))
+if a==0 or b==0:
+    print("value must not be zero")
+else:
+    while b != 0:
+        a,b=b,a%b #a=b,b==a%b
+    
+    print("Gcd is : ",a)
